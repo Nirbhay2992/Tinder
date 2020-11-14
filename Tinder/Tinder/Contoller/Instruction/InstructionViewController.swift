c1d311b3f54b8a8fc1b25220fbdad497f7417f3c
Nirbhay Singh <nirbhay2992@gmail.com>
2020-11-14 12:56:57 +0530

    1) Instruction screen is designed. 2) Choose gender screen is designed.

---
+++ Tinder/Tinder/Contoller/Instruction/InstructionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InstructionViewController.swift
+//  Tinder
+//
+//  Created by Nirbhay Singh on 14/11/20.
+//  Copyright © 2020 Nirbhay Singh. All rights reserved.
+//
+
+import UIKit
+
+class InstructionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func agreeButtonAction(_ sender: Any) {
+    }
+    
+}
